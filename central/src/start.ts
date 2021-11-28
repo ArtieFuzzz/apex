38c01857e94023c2c4f33e0416d4f8a9d2f274eb
♥ ArtieFuzzz ♥ <48877462+ArtieFuzzz@users.noreply.github.com>
2021-11-28 14:19:06 +1100

    Update start.ts

--- central/src/start.ts
+++ central/src/start.ts
@@ -1,17 +1,18 @@
 import { container } from '#apex/api'
 import Logger from '#apex/api/singletons/Logger'
 
-(async () => {
-	const logger = Logger.getChildLogger({
-		name: 'Apex: Central Control'
-	})
+const logger = Logger.getChildLogger({
+	name: 'Apex: Central Control'
+});
 
+(async () => {
 	try {
 		await container.load()
 		
 		logger.info('Apex has started!')
 	} catch (e) {
 		logger.error(e)
+
 		process.exit(1)
 	}
 })()
